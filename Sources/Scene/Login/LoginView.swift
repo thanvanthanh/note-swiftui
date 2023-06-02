11935250cf9114395342973c59c268b154d99499
Sotatek-ThanhThan <thanh.than@sotatek.com>
2023-06-02 18:31:45 +0700

    Fix project file

---
+++ Sources/Scene/Login/LoginView.swift
@@ -0,0 +1,20 @@
+//
+//  LoginView.swift
+//  NoteApp
+//
+//  Created by Thân Văn Thanh on 02/06/2023.
+//
+
+import SwiftUI
+
+struct LoginView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct LoginView_Previews: PreviewProvider {
+    static var previews: some View {
+        LoginView()
+    }
+}
